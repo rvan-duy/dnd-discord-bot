c6764f0f093231642b9b4ae391065b8c81feb4a5
Ruben van Duyneveldt <ruben.vanduyneveldt@stedin.net>
2025-02-08 21:55:40 +0100

    testing db connection

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import { registerCommands } from './utils/command-loader.js';
+import { Pool } from 'pg';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on(Events.ClientReady, readyClient => {
   console.log(`Logged in as ${readyClient.user.tag}!`);
+
+  // setup postgressql connection with DATABASE_URL
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // do a query to list all tables in the database
+  pool.query('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\'', (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Tables:');
+    for (const row of res.rows) {
+      console.log(row.table_name);
+    }
+  });
+
 });
 
 client.on(Events.InteractionCreate, async interaction => {
